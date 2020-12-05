2e213fdfcf99ec6623704315ab71fe410e53fe22
dotjsNet <payincent@gmail.com>
2020-12-05 11:29:46 +0500

    Excluded map for multiple images

--- config.php
+++ config.php
@@ -9,16 +9,16 @@
 // | Copyright (c) 2016 WoWonder. All rights reserved.
 // +------------------------------------------------------------------------+
 // MySQL Hostname
-$sql_db_host = "p:mysql8-service";
+$sql_db_host = "localhost";
 // MySQL Database User
 $sql_db_user = "root";
 // MySQL Database Password
-$sql_db_pass = ".sweetpwd.";
+$sql_db_pass = "";
 // MySQL Database Name
-$sql_db_name = "my_db";
+$sql_db_name = "vivlio";
 
 // Site URL
-$site_url = "https://www.vivlio.cy"; // e.g (http://example.com)
+$site_url = "http://localhost/vivilio"; // e.g (http://example.com)
 
 // Purchase code
 $purchase_code = "c221d476-f4df-4888-b09e-220bd64bccf4"; // Your purchase code, don't give it to anyone. 
